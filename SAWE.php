8fc9686c3b2d8c51b78a1fe4b443becb699bc236
boyevul <boyevul@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-17 02:22:24 +0000

    1.4 - 2/15/2013: added default styles (that can be optionally loaded) - styled the options page, added relevant information. If your theme doesn't support post thumbnails, SAWE will enable them.
    git-svn-id: https://plugins.svn.wordpress.org/simple-announcement-with-exclusion-sawe/trunk@669065 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- SAWE.php
+++ SAWE.php
@@ -3,7 +3,7 @@
 	Plugin Name: Simple Announcement With Exclusion (SAWE)
 	Plugin URI: https://github.com/boyevul/SAWE
 	Description: Designate a category for announcements to show in a widget while excluding it from the main loop.
-	Version: 1.3
+	Version: 1.4
 	Author: Matthew Trevino
 	Author URI: https://github.com/boyevul/
 	License: A "Slug" license name e.g. GPL2
@@ -37,7 +37,7 @@
 */
 
 
-//	Last update February 14th, 2013 - 6:04 AM
+//	Last update February 16th, 2013 - 8:04 PM
 
 
 // SAWE-000
@@ -46,15 +46,18 @@
 	register_deactivation_hook( __FILE__, "simple_announcement_with_exclusion_uninstall" );
 
 	function simple_announcement_with_exclusion_install() {
+		
+		// core settings
 		add_option("simple_announcement_with_exclusion_0","","Wrapper");
 		add_option("simple_announcement_with_exclusion_1","","Category");
-		add_option("simple_announcement_with_exclusion_2","","Option 2");
+		add_option("simple_announcement_with_exclusion_2","3","Option 2");
 		add_option("simple_announcement_with_exclusion_3","","Option 3");
 		add_option("simple_announcement_with_exclusion_3_2","","Option 3.2");
 		add_option("simple_announcement_with_exclusion_4","no","Option 4");
 		add_option("simple_announcement_with_exclusion_4_2","yes","Option 4.2");
 		add_option("simple_announcement_with_exclusion_4_3","nothing","Option 4.3");
 		add_option("simple_announcement_with_exclusion_5","yes","Option 5");
+		add_option("simple_announcement_with_exclusion_6","no","Option 6");
 		add_option("simple_announcement_with_exclusion_delete_on_deactivate","no","Delete on deactivate?");
 	}
 
@@ -72,8 +75,10 @@
 			delete_option("simple_announcement_with_exclusion_4_2");
 			delete_option("simple_announcement_with_exclusion_4_3");
 			delete_option("simple_announcement_with_exclusion_5");
+			delete_option("simple_announcement_with_exclusion_6");
 			delete_option("simple_announcement_with_exclusion_delete_on_deactivate");
 		} else { }
+		
 	}
 
 	
@@ -96,6 +101,7 @@
 				$_REQUEST["simple_announcement_with_exclusion_4_2"] ||
 				$_REQUEST["simple_announcement_with_exclusion_4_3"] ||
 				$_REQUEST["simple_announcement_with_exclusion_5"] ||
+				$_REQUEST["simple_announcement_with_exclusion_6"] ||
 				$_REQUEST["simple_announcement_with_exclusion_delete_on_deactivate"]
 				
 			) {
@@ -109,6 +115,7 @@
 			update_option("simple_announcement_with_exclusion_4_2",$_REQUEST["simple_announcement_with_exclusion_4_2"]);
 			update_option("simple_announcement_with_exclusion_4_3",$_REQUEST["simple_announcement_with_exclusion_4_3"]);
 			update_option("simple_announcement_with_exclusion_5",$_REQUEST["simple_announcement_with_exclusion_5"]);
+			update_option("simple_announcement_with_exclusion_6",$_REQUEST["simple_announcement_with_exclusion_6"]);
 			update_option("simple_announcement_with_exclusion_delete_on_deactivate",$_REQUEST["simple_announcement_with_exclusion_delete_on_deactivate"]);
 			$simple_announcement_with_exclusion_was_updated = true;
 			
@@ -132,121 +139,102 @@
 		$default_simple_announcement_with_exclusion_4_2 = get_option("simple_announcement_with_exclusion_4_2");
 		$default_simple_announcement_with_exclusion_4_3 = get_option("simple_announcement_with_exclusion_4_3");
 		$default_simple_announcement_with_exclusion_5 = get_option("simple_announcement_with_exclusion_5");
+		$default_simple_announcement_with_exclusion_6 = get_option("simple_announcement_with_exclusion_6");
 		$default_simple_announcement_with_exclusion_delete_on_deactivate = get_option("simple_announcement_with_exclusion_delete_on_deactivate");
 		
 		echo "
-		<form method=\"post\">
-		<table>
-		<tr>
-			<td><strong>Widget settings</strong></td>
-			<td><hr /></td>
-		</tr>	
-		<tr>
-			<td><label for\"simple_announcement_with_exclusion_0\">Div class name (optional):</label></td>
-			<td><input type=\"text\" name=\"simple_announcement_with_exclusion_0\" value=\"",$default_simple_announcement_with_exclusion_0,"\" /></td>
-		</tr>
-		<tr>
-			<td><strong>Category settings</strong></td>
-			<td><hr /></td>
-		</tr>
-		<tr>
-			<td><label for=\"simple_announcement_with_exclusion_1\">Category:</label></td>
-			<td><select name=\"simple_announcement_with_exclusion_1\" />";
-			
+		<form method=\"post\" class=\"SAWE_settings\">
+
+		<label for=\"simple_announcement_with_exclusion_0\"><span class=\"SAWE_settings_title\">Div class name (optional):</span>
+		<input type=\"text\" name=\"simple_announcement_with_exclusion_0\" value=\"",$default_simple_announcement_with_exclusion_0,"\" />
+		</label>
+
+		<label for=\"simple_announcement_with_exclusion_1\"><span class=\"SAWE_settings_title\">Category:</span>
+		<select name=\"simple_announcement_with_exclusion_1\">";
 				$sawe_categories=  get_categories(); 
 				foreach ($sawe_categories as $sawe_category) {
 					echo "<option value=\"",$sawe_category->cat_ID,"\"";
-					if ($sawe_category->cat_ID == $default_simple_announcement_with_exclusion_1) { echo "selected=\"selected\""; }
-					echo ">",$sawe_category->cat_name," - ",$sawe_category->category_count,"(",$sawe_category->cat_ID,")</option>";
+					if ($sawe_category->cat_ID == $default_simple_announcement_with_exclusion_1) { echo " selected=\"selected\""; }
+					echo ">",$sawe_category->cat_name," - ",$sawe_category->category_count,"</option>";
 				}
 				
 		echo "
-		</select></td>
-		</tr>
-		<tr>
-			<td><label for=\"simple_announcement_with_exclusion_2\">Number of posts:</label></td>
-			<td><input type=\"text\" name=\"simple_announcement_with_exclusion_2\" value=\"",$default_simple_announcement_with_exclusion_2,"\" /></td>
-		</tr>
-		<tr>
-			<td><strong>Post order and amount</strong></td>
-			<td><hr /></td>
-		</tr>
-		<tr>
-			<td><label for=\"simple_announcement_with_exclusion_3\">Order by:</label></td>
-			<td><select name=\"simple_announcement_with_exclusion_3\">
-				<option value=\"date\""; if ($default_simple_announcement_with_exclusion_3 == "date") { echo "selected=\"selected\""; } echo ">Date</option>
-				<option value=\"title\""; if ($default_simple_announcement_with_exclusion_3 == "title") { echo "selected=\"selected\""; } echo ">Title</option>
-				<option value=\"rand\""; if ($default_simple_announcement_with_exclusion_3 == "rand") { echo "selected=\"selected\""; } echo ">Random</option>
-			</select>
-			</td>
-		</tr>
-		<tr>
-			<td><label for=\"simple_announcement_with_exclusion_3_2\">Posts order:</label></td>
-			<td><select name=\"simple_announcement_with_exclusion_3_2\">
-				<option value=\"ASC\""; if ($default_simple_announcement_with_exclusion_3_2 == "ASC") { echo "selected=\"selected\""; } echo ">Ascending</option>
-				<option value=\"DESC\""; if ($default_simple_announcement_with_exclusion_3_2 == "DESC") { echo "selected=\"selected\""; } echo ">Descending</option>
-			</select>
-			</td>
-		</tr>
-		<tr>
-			<td><strong>Post display</strong></td>
-			<td><hr /></td>
-		</tr>";
+		</select>
+		</label>
+
+		<label for=\"simple_announcement_with_exclusion_2\"><span class=\"SAWE_settings_title\">Number of posts:</span>
+		<input type=\"text\" name=\"simple_announcement_with_exclusion_2\" value=\"",$default_simple_announcement_with_exclusion_2,"\" />
+		</label>
+		
+		<label for=\"simple_announcement_with_exclusion_3\"><span class=\"SAWE_settings_title\">Order by:</span>
+		<select name=\"simple_announcement_with_exclusion_3\">
+			<option value=\"date\""; if ($default_simple_announcement_with_exclusion_3 == "date") { echo " selected=\"selected\""; } echo ">Date</option>
+			<option value=\"title\""; if ($default_simple_announcement_with_exclusion_3 == "title") { echo " selected=\"selected\""; } echo ">Title</option>
+			<option value=\"rand\""; if ($default_simple_announcement_with_exclusion_3 == "rand") { echo " selected=\"selected\""; } echo ">Random</option>
+		</select>
+		</label>	
+		
+
+		<label for=\"simple_announcement_with_exclusion_3_2\"><span class=\"SAWE_settings_title\">Posts order:</span>
+		<select name=\"simple_announcement_with_exclusion_3_2\">
+			<option value=\"ASC\""; if ($default_simple_announcement_with_exclusion_3_2 == "ASC") { echo " selected=\"selected\""; } echo ">Ascending</option>
+			<option value=\"DESC\""; if ($default_simple_announcement_with_exclusion_3_2 == "DESC") { echo " selected=\"selected\""; } echo ">Descending</option>
+		</select>
+		</label>
+		
+		";
 		
 		// If thumbnail support isn"t enabled on the current theme, don"t bother giving the option to show thumbnails.
 		if ( (function_exists("has_post_thumbnail")) ) {
 			echo "
-			<tr>
-				<td><label for=\"simple_announcement_with_exclusion_4\">Thumbnails?</label></td>
-				<td><select name=\"simple_announcement_with_exclusion_4\">
-					<option value=\"yes\""; if ($default_simple_announcement_with_exclusion_4 == "yes") { echo "selected=\"selected\""; } echo ">Yes</option>
-					<option value=\"no\""; if ($default_simple_announcement_with_exclusion_4 == "no") { echo "selected=\"selected\""; } echo ">No</option>
-				</select>
-			</tr>";
+			<label for=\"simple_announcement_with_exclusion_4\"><span class=\"SAWE_settings_title\">Thumbnails?</span>
+			<select name=\"simple_announcement_with_exclusion_4\">
+				<option value=\"yes\""; if ($default_simple_announcement_with_exclusion_4 == "yes") { echo " selected=\"selected\""; } echo ">Yes</option>
+				<option value=\"no\""; if ($default_simple_announcement_with_exclusion_4 == "no") { echo " selected=\"selected\""; } echo ">No</option>
+			</select>
+			</label>
+			";
 			}
 		
 		echo "
-		<tr>
-				<td><label for=\"simple_announcement_with_exclusion_4_2\">Show titles?</label></td>
-				<td><select name=\"simple_announcement_with_exclusion_4_2\">
-					<option value=\"yes\""; if ($default_simple_announcement_with_exclusion_4_2 == "yes") { echo "selected=\"selected\""; } echo ">Yes</option>
-					<option value=\"no\""; if ($default_simple_announcement_with_exclusion_4_2 == "no") { echo "selected=\"selected\""; } echo ">No</option>
-				</select>
-		</tr>
-		<tr>
-				<td><label for=\"simple_announcement_with_exclusion_4_3\">Show post excerpt, full content, or nothing?</label></td>
-				<td><select name=\"simple_announcement_with_exclusion_4_3\">
-					<option value=\"nothing\""; if ($default_simple_announcement_with_exclusion_4_3 == "nothing") { echo "selected=\"selected\""; } echo ">Nothing</option>
-					<option value=\"excerpt\""; if ($default_simple_announcement_with_exclusion_4_3 == "excerpt") { echo "selected=\"selected\""; } echo ">Excerpt</option>
-					<option value=\"content\""; if ($default_simple_announcement_with_exclusion_4_3 == "content") { echo "selected=\"selected\""; } echo ">Content</option>
-				</select>
-		</tr>
-		<tr>
-			<td><strong>Exclusion rules</strong></td>
-			<td><hr /></td>
-		</tr>
-		<tr>
-			<td><label for=\"simple_announcement_with_exclusion_5\">Exclude posts from main loop?:</label></td>
-			<td><select name=\"simple_announcement_with_exclusion_5\">
-				<option value=\"yes\""; if ($default_simple_announcement_with_exclusion_5 == "yes") { echo "selected=\"selected\""; } echo ">Yes</option>
-				<option value=\"no\""; if ($default_simple_announcement_with_exclusion_5 == "no") { echo "selected=\"selected\""; } echo ">No</option>
-			</select></td>
-		</tr>
-		<tr>
-			<td><strong>Data retention</strong></td>
-			<td><hr /></td>
-		</tr>
-		<tr>
-			<td><label for=\"simple_announcement_with_exclusion_delete_on_deactivate\">Delete options on deactivation?:</label></td>
-			<td><select name=\"simple_announcement_with_exclusion_delete_on_deactivate\">
-				<option value=\"yes\""; if ($default_simple_announcement_with_exclusion_delete_on_deactivate == "yes") { echo "selected=\"selected\""; } echo ">Yes</option>
-				<option value=\"no\""; if ($default_simple_announcement_with_exclusion_delete_on_deactivate == "no") { echo "selected=\"selected\""; } echo ">No</option>
-			</select></td>
-		</tr>
-		<tr>
-			<td><input type=\"submit\" name=\"submit\" value=\"Save\" /></td>
-		</tr>
-		</table>
+			<label for=\"simple_announcement_with_exclusion_4_2\"><span class=\"SAWE_settings_title\">Show titles?</span>
+			<select name=\"simple_announcement_with_exclusion_4_2\">
+				<option value=\"yes\""; if ($default_simple_announcement_with_exclusion_4_2 == "yes") { echo " selected=\"selected\""; } echo ">Yes</option>
+				<option value=\"no\""; if ($default_simple_announcement_with_exclusion_4_2 == "no") { echo " selected=\"selected\""; } echo ">No</option>
+			</select>
+			</label>
+				<label for=\"simple_announcement_with_exclusion_4_3\"><span class=\"SAWE_settings_title\">Show post excerpt, full content, or nothing?</span>
+			<select name=\"simple_announcement_with_exclusion_4_3\">
+				<option value=\"nothing\""; if ($default_simple_announcement_with_exclusion_4_3 == "nothing") { echo " selected=\"selected\""; } echo ">Nothing</option>
+				<option value=\"excerpt\""; if ($default_simple_announcement_with_exclusion_4_3 == "excerpt") { echo " selected=\"selected\""; } echo ">Excerpt</option>
+				<option value=\"content\""; if ($default_simple_announcement_with_exclusion_4_3 == "content") { echo " selected=\"selected\""; } echo ">Content</option>
+			</select>
+			</label>
+				
+			<label for=\"simple_announcement_with_exclusion_5\"><span class=\"SAWE_settings_title\">Exclude posts from main loop?:</span>
+			<select name=\"simple_announcement_with_exclusion_5\">
+				<option value=\"yes\""; if ($default_simple_announcement_with_exclusion_5 == "yes") { echo " selected=\"selected\""; } echo ">Yes</option>
+				<option value=\"no\""; if ($default_simple_announcement_with_exclusion_5 == "no") { echo " selected=\"selected\""; } echo ">No</option>
+			</select>
+			</label>
+
+			<label for=\"simple_announcement_with_exclusion_6\"><span class=\"SAWE_settings_title\">Include default CSS?:</span>
+			<select name=\"simple_announcement_with_exclusion_6\">
+				<option value=\"yes\""; if ($default_simple_announcement_with_exclusion_6 == "yes") { echo " selected=\"selected\""; } echo ">Yes</option>
+				<option value=\"no\""; if ($default_simple_announcement_with_exclusion_6 == "no") { echo " selected=\"selected\""; } echo ">No</option>
+			</select>
+			</label>
+
+			<label for=\"simple_announcement_with_exclusion_delete_on_deactivate\"><span class=\"SAWE_settings_title\">Delete options on deactivation?:</span>
+			<select name=\"simple_announcement_with_exclusion_delete_on_deactivate\">
+				<option value=\"yes\""; if ($default_simple_announcement_with_exclusion_delete_on_deactivate == "yes") { echo " selected=\"selected\""; } echo ">Yes</option>
+				<option value=\"no\""; if ($default_simple_announcement_with_exclusion_delete_on_deactivate == "no") { echo " selected=\"selected\""; } echo ">No</option>
+			</select>
+			</label>
+			
+			
+			<input type=\"submit\" name=\"submit\" value=\"Save\" />
+
 		</form>";
 	}
 	
@@ -259,18 +247,28 @@
 // Create the information page for this plugin.
 	add_action("admin_menu", "simple_announcement_with_exclusion_add_options_page");
 	
+	if (is_admin() ) {
+		wp_register_style( 'SAWEStylesheet', plugins_url('style.css', __FILE__) );
+		wp_enqueue_style( 'SAWEStylesheet' );
+	}
+	
+	if (!is_admin() && get_option("simple_announcement_with_exclusion_6") === "yes") {
+		wp_register_style( 'SAWEDefaultStylesheet', plugins_url('default.css', __FILE__) );
+		wp_enqueue_style( 'SAWEDefaultStylesheet' );
+	}
+	
 	function simple_announcement_with_exclusion_add_options_page() {
 		add_options_page("SAWE", "SAWE", "manage_options", "simple_announcement_with_exclusion", "simple_announcement_with_exclusions_page_content");
 	}
 
 // Display the information on the page that was created.
 	function simple_announcement_with_exclusions_page_content() { 
-		echo "	<div class=\"wrap\">
+		echo "	<div class=\"SAWE_container\">
 				<h2>Simple Announcement With Exclusion (SAWE)</h2>
-				Designate a category for announcements to show in a widget while excluding it from the main loop.
+				<p>Designate a category for announcements to show in a widget while excluding it from the main loop.</p>
 				
 				<blockquote>
-				<em>Settings</em><br />
+				
 					";
 						
 					if ($_REQUEST["submit"]) { 
@@ -279,6 +277,46 @@
 					print_simple_announcement_with_exclusion_form();
 					
 				echo "</blockquote>
+				
+				<div class=\"SAWE_information\">
+				<strong>Shortcode</strong>
+				<p>Use the shortcode <strong>[SAWE]</strong> on a post or a page to display the SAWE loop.</p>
+				</div>
+				
+				<div class=\"SAWE_information\">
+				<strong>About Default Styles</strong>
+				<p>You have the option of loading the default styles for the widget/shortcode area, which will style 
+				the output of SAWE to make the content fit in the best way possible.  However, since each theme is different, 
+				and styling can be tricky when it comes to preparing for the potential thousands of different themes this 
+				plugin could interact with, I can't say that it will look great <strong>in every situation</strong>.</p>
+				<p>Should you find that loading the default styles makes it stick out in a way that is unsastisfactory, I'd 
+				suggest simply disabling that option, adding a class name, and editing the CSS manually.</p>
+				</div>
+				
+				<div class=\"SAWE_information\">
+				<strong>Post thumbnails</strong>
+				<p>";
+				
+				if ( (function_exists("has_post_thumbnail")) ) { 
+					echo "Looks like post thumbnails were already enabled for this theme!";
+				} else {
+					add_theme_support( 'post-thumbnails' );
+					echo "Post thumbnails has been enabled for your theme.  You may add them from your post editing screen.";
+				}
+				
+				echo "</p></div>
+				
+				<div class=\"SAWE_information\">
+				<strong>Changelog</strong>
+				<p>You can view the official changelog for this plugin at its page on Github (located here: <a href=\"https://github.com/boyevul/sawe\">https://github.com/boyevul/sawe</a>)</p>
+				</div>
+				
+				<div class=\"SAWE_information\">
+				<strong>Thanks for using SAWE!</strong>
+				<p>Hopefully you've been able to find some use out of this little plugin!</p>
+				</div>
+				
+				
 				</div>
 			";			
 	}
@@ -314,9 +352,12 @@
 			$SAWE_4_2_w = ( get_option("simple_announcement_with_exclusion_4_2") );
 			$SAWE_4_3_w = ( get_option("simple_announcement_with_exclusion_4_3") );
 			$SAWE_5_w = ( get_option("simple_announcement_with_exclusion_5") );
+			$SAWE_6_w = ( get_option("simple_announcement_with_exclusion_6") );
 			
 			if ($SAWE_1_w != "" && $SAWE_2_w != "" && $SAWE_3_w != "") {
-				if ($SAWE_0_w != "") { echo "<div class=\"$SAWE_0_w\">"; }
+				echo "<div class=\"";
+				if ($SAWE_0_w != "") { echo "$SAWE_0_w"; }
+				echo "\" id=\"SAWE_widget\">";
 				global $post;
 				$tmp_post = $post;
 				$args = array( 
@@ -396,9 +437,12 @@
 			$SAWE_4_2_sc = ( get_option("simple_announcement_with_exclusion_4_2") );
 			$SAWE_4_3_sc = ( get_option("simple_announcement_with_exclusion_4_3") );
 			$SAWE_5_sc = ( get_option("simple_announcement_with_exclusion_5") );
+			$SAWE_6_sc = ( get_option("simple_announcement_with_exclusion_6") );
 			
 			if ($SAWE_1_sc != "" && $SAWE_2_sc != "" && $SAWE_3_sc != "") {
-				if ($SAWE_0_sc != "") { echo "<div class=\"$SAWE_0_sc\">"; }
+				echo "<div class=\"";
+				if ($SAWE_0_w != "") { echo "$SAWE_0_sc"; }
+				echo "\" id=\"SAWE_shortcode\">";
 				global $post;
 				$tmp_post = $post;
 				$args = array( 
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link:
 Tags: widget, category, hide, mini loop, shortcode
 Requires at least: 3.3
 Tested up to: 3.5.1
-Stable tag: 1.3
+Stable tag: 1.4
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -16,7 +16,7 @@ SAWE is a plugin to set aside a single category as an announcements section (or 
 
 == Installation ==
 
-1. Upload sawe.php to your /wp-content/plugins directory.
+1. Upload /SAWE/ to your /wp-content/plugins directory.
 2. Navigate to your plugins menu in your Wordpress admin.
 3. Activate it.
 4. You'll find settings for the plugin under Settings->SAWE.
@@ -34,8 +34,14 @@ Place the widget (optional) or use the shortcode [sawe] (optional).
 
 == Changelog ==
 
-https://github.com/boyevul/sawe
+
+1.0 - 2/13/2013: initial release.
+1.1 - 2/13/2013: cleaned up settings page.
+1.2 - 2/13/2013: added [sawe] shortcode (to display SAWE on a post or a page without having to use the widget).
+1.3 - 2/14/2013: added ability to show excerpt or full post content. Cleaned up code a bit.
+1.4 - 2/15/2013: added default styles (that can be optionally loaded) - styled the options page, added relevant information.
+If your theme doesn't support post thumbnails, SAWE will enable them.
+
 
 == Upgrade notice ==
 
-https://github.com/boyevul/sawe
